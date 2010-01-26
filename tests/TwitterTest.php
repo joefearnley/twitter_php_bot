a38cabcc0ef44af0519d4e60dff759fb511a8994
Joe Fearnley <joe.fearnley@gmail.com>
2010-01-25 20:55:11 -0500

    extended class to test case

--- tests/TwitterTest.php
+++ tests/TwitterTest.php
@@ -9,14 +9,21 @@
  * I modified the Twitter class by adding some additional
  * functions. These are the units tests for those functions.
  */ 
-class TwitterTest {
 
-	private twitter;
+require_once 'PHPUnit/Framework.php';
+
+class TwitterTest extends PHPUnit_Framework_TestCase {
+
+	private $twitter = null;
 
 	function setUp() {
+		include_once('../twitter/Twitter.php');
+		$twitter = new twitter(); 
 	}
 
 	function testSearch() {
+
+		
 	}
 
     function testSearchSingle() {
