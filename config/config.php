248d1d1b43124a64d47b24b4f3aaecc032b3e559
Joe Fearnley <joe.fearnley@gmail.com>
2010-01-27 21:01:13 -0500

    added false credentials

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-	$config['username'] = '';
-	$config['password'] = '';
-	$config['search_terms'] = array('','','');
+	$config['username'] = 'username';
+	$config['password'] = 'password';
+	$config['search_terms'] = array('search term 1','search term 2','search term 3');
 ?>
