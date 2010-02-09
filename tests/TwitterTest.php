baf9c7ae586a3086e1c144417ce773d1b061c22d
Joe Fearnley <joe.fearnley@gmail.com>
2010-02-08 23:14:27 -0500

    tests are failin' but they're there

--- tests/TwitterTest.php
+++ tests/TwitterTest.php
@@ -11,28 +11,52 @@
  */
 
 require_once 'PHPUnit/Framework.php';
+require_once '../twitter/Twitter.php';
+require_once '../config/Config.php';	
 
-class TwitterTest extends PHPUnit_Framework_TestCase {
-
-	private $twitter = null;
-
+class TwitterTest extends PHPUnit_Framework_TestCase 
+{
+	protected $twitter = null;
+	protected $summize = null;
+	protected $config = null;
+	
 	function setUp() 
-	{
-		include_once('../twitter/Twitter.php');
-		include_once('../config/TwitterBotConfig.php');		
-
-		$config = TwitterBotConfig();		
-		$this->assertNotNull($config);
-		$this->assertEquals($config->username, 'j3fearnl');
-
-
-//		$twitter = new twitter($config['username'], $config['password']);
-//		$avail = $twitter->twitterAvailable();
-//		$this->assertTrue($avail);
+	{	
+		$this->config = Config::getInstance();
+		$this->twitter = new twitter();
+		$this->summize = new summize();
+		
+		$avail = $this->twitter->twitterAvailable();
+		$this->assertTrue($avail);
 	}
 
+	public function tearDown()
+	{
+		unset($this->config);
+		unset($this->twitter);
+		unset($this->summize);
+	}
+	
 	function testSearch() 
 	{
+		$search_string = '';
+		$search_terms = $this->config->getSearchTerms();
+	
+		for($i = 0; $i < sizeof($search_terms); $i++) {
+			$search_string = $search_string . '+' . $search_terms[$i];
+		}
+
+		if($search_string[0] == '+') {
+			$search_string[0] = '';
+		}
+
+		$search_results = $this->summize->search($search_string);
+		$this->assertTrue($search_results);
+		
+		foreach($search_results->results as $tweet) {
+			$this->assertTrue($tweet);
+			$this->assertTrue(strpos($tweet, 'hockey') === true);
+		}		
 	}
 
     function testSearchSingle() 
@@ -50,7 +74,6 @@ class TwitterTest extends PHPUnit_Framework_TestCase {
 	function testPantsOnTheGround() 
 	{
 	}
-
 }
 
 ?>
