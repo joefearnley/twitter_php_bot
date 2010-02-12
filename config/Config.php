f9f2a6603292595c78c53b0f98ed3c1e1a131211
Joe Fearnley <joe.fearnley@gmail.com>
2010-02-12 18:45:27 -0500

    changed search terms

--- config/Config.php
+++ config/Config.php
@@ -9,9 +9,9 @@ class Config {
 
 	private function __construct() 
 	{ 
-		$this->username = 'j3fearnl';
- 		$this->password = 'ymra4785';	
-		$this->search_terms = array('hockey');
+		$this->username = '';
+ 		$this->password = '';	
+		$this->search_terms = array('');
 	}
 
 	public static function getInstance()
