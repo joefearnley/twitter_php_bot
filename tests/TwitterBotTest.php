7b4ae3c28ff9061369be3ac96f15e9ea3b7e27fa
Joe Fearnley <joe.fearnley@gmail.com>
2010-02-11 21:00:15 -0500

    added string format test

--- tests/TwitterBotTest.php
+++ tests/TwitterBotTest.php
@@ -8,7 +8,40 @@
  * 
  * These are the tests for the TwitterBot class.
  */ 
-class TwitterBotTest  {	
+
+require_once 'PHPUnit/Framework.php';
+require_once '../twitter/Twitter.php';
+require_once '../twitter/TwitterBot.php';
+require_once '../config/Config.php';
+
+class TwitterBotTest extends PHPUnit_Framework_TestCase 
+{	
+	protected $twitter = null;
+	protected $summize = null;
+	protected $config = null;
+	protected $twitter_bot = null;
+	
+	public function setUp() 
+	{	
+		$this->config = Config::getInstance();
+		$this->twitter = new twitter();
+		$this->summize = new summize();
+		$this->twitter_bot = new TwitterBot();
+	}
+
+	public function tearDown()
+	{
+		$avail = $this->twitter->twitterAvailable();
+		$this->assertTrue($avail);
+	}
+
+	public function testFormatSearchString($search_string = '') {
+		$search_terms = array('hockey','peter klima','cheifs');
+		$this->assertEquals($this->twitter_bot->formatSearchString($search_terms), 'hockey+peter klima+cheifs');
+
+		$search_terms = array('looking','for','hookers');
+		$this->assertEquals($this->twitter_bot->formatSearchString($search_terms), 'looking+for+hookers');
+	}
 }
 
 ?>
