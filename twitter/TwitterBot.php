6eb7fe4e77a4b4344b7bd841989698a178f53889
Joe Fearnley <joe.fearnley@gmail.com>
2010-02-14 10:55:41 -0500

    fixed tenary operation

--- twitter/TwitterBot.php
+++ twitter/TwitterBot.php
@@ -34,7 +34,7 @@ class TwitterBot extends Twitter {
 			$friendship = $this->showFriendship($source_user, $target_user);
 
 			if(!$friendship->relationship->target->followed_by) {
-				$this->followUser($user_info->id, false)
+				$this->followUser($user_info->id, false);
 
 
 			}
@@ -47,7 +47,8 @@ class TwitterBot extends Twitter {
 		for($i = 0; $i < sizeof($search_terms); $i++) {
 			$search_string = $search_string . '+' . $search_terms[$i];
 		}
-		$search_string[0] = ($search_string[0] == '+') $search_string[0] = '' ? : $search_string[0];
+
+		$search_string[0] = ($search_string[0] == '+') ? '' : $search_string[0];
 		return trim($search_string);
 	}
 }
