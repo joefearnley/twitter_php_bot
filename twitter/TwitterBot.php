1c69f466c8f856275fe582f76b763c07de6455e6
Joe Fearnley <joe.fearnley@gmail.com>
2010-02-13 23:39:38 -0500

    added init function

--- twitter/TwitterBot.php
+++ twitter/TwitterBot.php
@@ -18,17 +18,34 @@ class TwitterBot extends Twitter {
 
 	function __construct($username, $password, $search_terms) 
 	{ 
-		
+		$this->username = $username;
+		$this->password = $password;
+		$this->search_terms = $search_terms;
 	}
 	
-	public function followSomePeople() {
-			
-	}
-	
-	public function formatSearchString($search_terms = array()) {	
+	public function init()
+	{
+		$search_terms = $this->search_terms;
+		$search_string = $this->formatSearchString($search_terms);
+		$search_results = $this->search(urlencode($search_string));
 
+		foreach($search_results->results as $tweet) {
+			// get the user info. (make function for this?)
+			$user_info = $this->twitter->showUser(false, false, false, $this->username);
+
+			// befriend them (make a function for this)...and test it.
+			
+		}
+	}
+
+	public function followUser()
+	{
+	}
+
+	public function formatSearchString($search_terms = array()) 
+	{
 		$search_string = '';
-	
+
 		for($i = 0; $i < sizeof($search_terms); $i++) {
 			$search_string = $search_string . '+' . $search_terms[$i];
 		}
