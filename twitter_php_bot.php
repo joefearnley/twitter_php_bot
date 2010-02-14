b19a7a9826cb9ba71b6ae1cb62c318315c7b47c3
Joe Fearnley <joe.fearnley@gmail.com>
2010-02-13 23:10:41 -0500

    changed the main file name

---
+++ twitter_php_bot.php
@@ -0,0 +1,17 @@
+<?php
+
+   /**
+    * update_twitter.php
+    *
+    * read in some parameters from the command line, authenticate to twitter, and
+    * follow users based on search parameter. A twitter "bot" if you will. Evil
+    * I suppose. So don't use it if that's what you believe.
+    * 
+    * @author joe fearnley
+    * @date 01/20/2010
+    * 
+    */
+
+	// pants on the ground
+
+?>
