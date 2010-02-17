bf6cb5104402340aa5416e434137f72a14765395
Joe Fearnley <joe.fearnley@gmail.com>
2010-02-16 20:48:46 -0500

    added tests for init

--- tests/TwitterBotTest.php
+++ tests/TwitterBotTest.php
@@ -10,31 +10,41 @@
  */ 
 
 require_once 'PHPUnit/Framework.php';
-require_once '../twitter/Twitter.php';
 require_once '../twitter/TwitterBot.php';
-require_once '../config/Config.php';
 
 class TwitterBotTest extends PHPUnit_Framework_TestCase 
 {
-	protected $twitter = null;
-	protected $config = null;
-	protected $twitter_bot = null;
+	private $twitter_bot;
  
 	public function setUp() 
-	{	
-		$this->config = Config::getInstance();
-		$this->twitter = new twitter();
-		$this->twitter_bot = new TwitterBot();
+	{
+		$this->twitter_bot = new TwitterBot('j3fearnl', '!B00ze!', array('hockey'));
 	}
 
 	public function tearDown()
 	{
-		unset($this->config);
-		unset($this->twitter);
 		unset($this->twitter_bot);
 	}
 
-	public function testFormatSearchString($search_string = '') {
+	public function testInit($search_string = '') 
+	{
+		// get the current number of friends
+		$number_of_friends = count($this->twitter_bot->showFriends());
+		$this->twitter_bot->init();
+		
+		// number of friends should be higher
+		$this->assertTrue($number_of_friends < count($this->twitter_bot->showFriends()));
+
+		// do another test with different search terms, then test friend count again.
+		$number_of_friends = count($this->twitter_bot->showFriends());
+		$this->twitter_bot->setSearchTerms(array('karate kid'));
+		$this->twitter_bot->init();
+
+		// should be following even more now 
+		$this->assertTrue($number_of_friends < count($this->twitter_bot->showFriends()));
+	}
+
+	public function testFormatSearchString() {
 		$search_terms = array('hockey','peter klima','cheifs');
 		$this->assertEquals($this->twitter_bot->formatSearchString($search_terms), 'hockey+peter klima+cheifs');
 
