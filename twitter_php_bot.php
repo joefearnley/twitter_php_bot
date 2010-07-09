7f2e382223774af4737e7caa50c394fd99f44543
Joe Fearnley <joe.fearnley@gmail.com>
2010-07-08 18:28:22 -0700

    Removed the tab, moving on to the next version.

--- twitter_php_bot.php
+++ twitter_php_bot.php
@@ -1,25 +1,25 @@
 <?php
 
-    /**
-     * twitter_php_bot.php
-     *
-     * Authenticate to twitter, and follow users based on search parameter. 
-     * A twitter "bot" if you will. Evil I suppose. So don't use it if that's what you believe.
-     * 
-     * @author joe fearnley
-     * @date 01/20/2010
-     * 
-     */
+ /**
+  * twitter_php_bot.php
+  *
+  * Authenticate to twitter, and follow users based on search parameter. 
+  * A twitter "bot" if you will. Evil I suppose. So don't use it if that's what you believe.
+  * 
+  * @author joe fearnley
+  * @date 01/20/2010
+  * 
+  */
 
-    require_once 'twitter/TwitterBot.php';
+require_once 'twitter/TwitterBot.php';
 
-    $username = '';
-    $password = '';
-    $search_terms = array('');
+$username = '';
+$password = '';
+$search_terms = array('');
 
-    $twitter_bot = new TwitterBot($username, $password, $search_terms);
-    $twitter_bot->init();
+$twitter_bot = new TwitterBot($username, $password, $search_terms);
+$twitter_bot->init();
     
-    print "\n\nYou are now following " . $twitter_bot->getNumberOfFollowees() . " twitter users.\n\n";
+echo "\n\nYou are now following " . $twitter_bot->getNumberOfFollowees() . " twitter users.\n\n";
 
 ?>
